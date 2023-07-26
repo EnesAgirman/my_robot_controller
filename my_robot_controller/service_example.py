ddd520f62673d45fe1e2cb4e23b62059c0b90443
Enes Agirman <enes.agirman@ug.bilkent.edu.tr>
2023-07-26 11:05:11 +0200

    finished the tutorial and it works great

--- my_robot_controller/service_example.py
+++ my_robot_controller/service_example.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
+from functools import partial
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 import math
 from rclpy.clock import Clock
+from turtlesim.srv import SetPen
 
 
 class ServiceExample(Node):
@@ -15,6 +17,10 @@ class ServiceExample(Node):
         super().__init__("service_example")
         self.mySub = self.create_subscription(Pose, "/turtle1/pose", self.pose_callback, 10)
         self.myPub = self.create_publisher(Twist, "/turtle1/cmd_vel", 10)
+        
+        self.previous_x = 0.0
+        self.previous_y = 0.0
+        
         self.get_logger().info("the turtle controller node has been succesfully created")
 
 
@@ -29,7 +35,39 @@ class ServiceExample(Node):
             cmd.linear.x = 5.0
             cmd.angular.z = 0.0
         
+        if self.previous_x <= 5.0  and pose.x > 5.0:
+            self.previous_x = pose.x
+            self.previous_y = pose.y
+            self.set_pen_callback(255, 255, 255, 3, 0)
+        elif self.previous_x >= 5.0  and pose.x < 5.0:
+            self.previous_x = pose.x
+            self.previous_y = pose.y
+            self.set_pen_callback(0, 255, 0, 3, 0)
         self.myPub.publish(cmd)
+        
+    
+    def set_pen_callback( self, r, g, b, width, off ):
+        client = self.create_client(SetPen, "/turtle1/set_pen")
+        while not client.wait_for_service(1.0):
+            self.get_logger().warn("waiting for service")
+        
+        request = SetPen.Request()
+        request.r = r
+        request.g = g
+        request.b = b
+        request.width = width
+        request.off = off
+        
+        
+        future = client.call_async(request)
+        future.add_done_callback(partial(self.future_callback))
+        
+    def future_callback(self, future):
+        try:
+            response = future.result()
+            self.get_logger().info("the service has been called")
+        except Exception as e:
+            self.get_logger().error("service call failed %r" % (e,))
         
         
 
